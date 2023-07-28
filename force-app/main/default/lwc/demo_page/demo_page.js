12184ed5b97e56b4537f3e387eba537ea8216a46
Shigeo Tejima <shigeo.tejima@gmail.com>
2023-07-29 04:17:37 +0900

    impl to show list of demo, and reflesh

--- force-app/main/default/lwc/demo_page/demo_page.js
+++ force-app/main/default/lwc/demo_page/demo_page.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import getDemos from '@salesforce/apex/DemoController.getDemos';
 
-export default class Demo_page extends LightningElement {}
+const DEMO_COLUMNS = [
+    { label: 'Name', fieldName: 'name', type: 'text' },
+];
+
+export default class Demo_page extends LightningElement {
+
+    demo_columns = DEMO_COLUMNS;
+
+    @wire(getDemos)
+    demos;
+
+    get existsDemos() {
+        return this.demos && this.demos.data;
+    }
+
+    handleReflesh() {
+        refreshApex(this.demos);
+    }
+}
