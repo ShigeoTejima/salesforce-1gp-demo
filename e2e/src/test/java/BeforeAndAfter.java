4023b8f8f7aa459b8e0c060ce00f0feeafaad9dd
Shigeo Tejima <shigeo.tejima@gmail.com>
2023-08-17 10:37:19 +0900

    add chromeOptions.arguments, and .env-local-default

--- e2e/src/test/java/BeforeAndAfter.java
+++ e2e/src/test/java/BeforeAndAfter.java
@@ -5,6 +5,7 @@ import gateway.SalesforceGateway;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
 import java.util.Optional;
 
 public class BeforeAndAfter {
@@ -12,25 +13,29 @@ public class BeforeAndAfter {
     @BeforeSuite
     public void beforeSuite() {
         // NOTE: デフォルトの.env-defaultをロードして、システムプロパティに設定する
-        Dotenv.configure()
-                .filename(".env-default")
-                .systemProperties()
-                .load();
+        loadSystemPropertiesFromDotenv(".env-default");
+
+        // NOTE: ローカルでの実行用の.env-local-defaultをロードして、システムプロパティに設定する. 設定があればデフォルトの設定値を上書きする
+        loadSystemPropertiesFromDotenv(".env-local-default");
 
         // NOTE: ローカルでの実行用の.env-localをロードして、システムプロパティに設定する. 設定があればデフォルトの設定値を上書きする
-        Dotenv.configure()
-                .filename(".env-local")
-                .ignoreIfMissing()
-                .systemProperties()
-                .load();
+        loadSystemPropertiesFromDotenv(".env-local");
 
         // NOTE: Chromeのみで検証しているため、Chromeに対する設定があれば設定している
         Optional.ofNullable(System.getProperty("chromeOptions.addArguments")).ifPresent(arguments -> {
             ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.addArguments(arguments);
+            List.of(arguments.split(" ")).stream().forEach(argument -> chromeOptions.addArguments(argument));
             Configuration.browserCapabilities = chromeOptions;
         });
 
+    }
+
+    private void loadSystemPropertiesFromDotenv(String filename) {
+        Dotenv.configure()
+                .filename(filename)
+                .ignoreIfMissing()
+                .systemProperties()
+                .load();
     }
 
     @BeforeSpec(tags = { "cleanDemo" })
