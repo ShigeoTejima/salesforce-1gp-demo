95539b7f8d9e9a18add7e5832c57b60c37b44ee2
Shigeo Tejima <shigeo.tejima@gmail.com>
2023-08-28 23:13:53 +0900

    fix bit

--- force-app/main/default/lwc/demo_page/demo_page.js
+++ force-app/main/default/lwc/demo_page/demo_page.js
@@ -1,56 +1,55 @@
-import { LightningElement, wire } from 'lwc';
-import { refreshApex } from '@salesforce/apex';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import getDemos from '@salesforce/apex/DemoController.getDemos';
+import { LightningElement, wire } from "lwc";
+import { refreshApex } from "@salesforce/apex";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import getDemos from "@salesforce/apex/DemoController.getDemos";
 
 const DEMO_COLUMNS = [
-    { label: 'Name', fieldName: 'name', type: 'text' },
-    { label: 'Description', fieldName: 'description', type: 'text' }
+  { label: "Name", fieldName: "name", type: "text" },
+  { label: "Description", fieldName: "description", type: "text" }
 ];
 
 export default class Demo_page extends LightningElement {
+  demo_columns = DEMO_COLUMNS;
 
-    demo_columns = DEMO_COLUMNS;
+  demos;
 
-    demos;
+  get existsDemos() {
+    return this.demos && this.demos.data;
+  }
 
-    get existsDemos() {
-        return this.demos && this.demos.data;
+  get demoData() {
+    return this.existsDemos ? this.demos.data : null;
+  }
+
+  @wire(getDemos)
+  getDemos(value) {
+    this.demos = value;
+    const { data, error } = value;
+    if (error) {
+      this.dispatchErrorToast();
+    } else if (data) {
+      // nop
     }
+  }
 
-    get demoData() {
-        return this.existsDemos ? this.demos.data : null;
-    }
+  handleReflesh() {
+    /*
+     * NOTE: 2023/7/30) 取得済みのthis.demosがerrorだった場合に、
+     * その後接続先のApexが正常に回復しても、refleshApexで正しい結果が反映されないように見えた
+     */
+    refreshApex(this.demos).catch(() => {
+      this.dispatchErrorToast();
+    });
+  }
 
-    @wire(getDemos)
-    getDemos(value) {
-        this.demos = value;
-        const { data, error } = value;
-        if (error) {
-            this.dispatchErrorToast();
-        } else if (data) {
-            // nop
-        }
-    }
-
-    handleReflesh() {
-        /*
-         * NOTE: 2023/7/30) 取得済みのthis.demosがerrorだった場合に、
-         * その後接続先のApexが正常に回復しても、refleshApexで正しい結果が反映されないように見えた
-         */
-        refreshApex(this.demos).catch(() => {
-            this.dispatchErrorToast();
-        });
-    }
-
-    // Helper
-    dispatchErrorToast() {
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title: 'Error loading Demo',
-                message: 'fail to fetch records of demo',
-                variant: 'error'
-            })
-        );
-}
+  // Helper
+  dispatchErrorToast() {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: "Error loading Demo",
+        message: "fail to fetch records of demo",
+        variant: "error"
+      })
+    );
+  }
 }
